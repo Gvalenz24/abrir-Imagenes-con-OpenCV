5537a42230e24ede87dabf1ae35bcf76f0e33722
Gvalenz24 <49285897+Gvalenz24@users.noreply.github.com>
2020-12-29 23:26:05 -0600

    ComentariosCodigo
    se agregaron comentarios en el código para su comprensión

--- ImagenCv2.py
+++ ImagenCv2.py
@@ -1,8 +1,13 @@
 import cv2
 
-imagen = cv2.imread('logo.jpg',0)
+imagen = cv2.imread('logo.jpg',0) 
+# La función imread de OpenCV carga una imagen del archivo especificado y la devuelve.
+# puedes cambiar 'logo.jpg' por alguna imagen de tu preferencia
+# el 0 cambia los colores a escala de grises, si se busca ver la imagen con los colores originales eliminar esta parte
 
 cv2.imshow('Imagen', imagen)
+# La función imshow de OpenCV muestra una imagen.
 cv2.imwrite('grises.jpg',imagen)
+#La función imwrite de OpenCV guarda la imagen.
 cv2.waitKey(0)
 cv2.destroyAllWindows()
